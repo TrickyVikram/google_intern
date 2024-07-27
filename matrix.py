7b2bba98f993f700adaf5c718e2a2013c00db946
TrickyVikram <vkumarsah999@gmail.com>
2024-07-27 13:28:57 +0530

    “add”

--- matrix.py
+++ matrix.py
@@ -1,18 +1,9 @@
-
-import cv2
 import numpy as np
 from scipy import misc
 import matplotlib.pyplot as plt
 
 
 i = misc.ascent()
-
-plt.grid(False)
-plt.gray()
-plt.axis('off')
-plt.imshow(i)
-plt.show()
-
 i_transformed = np.copy(i)
 size_x = i_transformed.shape[0]
 size_y = i_transformed.shape[1]
