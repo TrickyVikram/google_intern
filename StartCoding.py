6868cd2126db03489135a91eb98a1b4fcdaec84b
TrickyVikram <vkumarsah999@gmail.com>
2024-07-27 13:23:01 +0530

    “add”

--- StartCoding.py
+++ StartCoding.py
@@ -2,11 +2,17 @@
 import cv2
 import numpy as np
 from scipy import misc
+import matplotlib.pyplot as plt
+
+
 i = misc.ascent()
 
-import matplotlib.pyplot as plt
 plt.grid(False)
 plt.gray()
 plt.axis('off')
 plt.imshow(i)
 plt.show()
+
+i_transformed = np.copy(i)
+size_x = i_transformed.shape[0]
+size_y = i_transformed.shape[1]
