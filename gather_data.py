ef548fc0c0ec867b06758c444216ae53e70d4328
TrickyVikram <vkumarsah999@gmail.com>
2024-07-27 14:17:28 +0530

    “add”

--- gather_data.py
+++ gather_data.py
@@ -5,3 +5,21 @@ training_images=training_images.reshape(60000, 28, 28, 1)
 training_images = training_images/255.0
 test_images = test_images.reshape(10000, 28, 28, 1)
 test_images = test_images/255.0
+
+model = tf.keras.models.Sequential([
+tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+tf.keras.layers.MaxPooling2D(2, 2),
+#Add another convolution
+tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+tf.keras.layers.MaxPooling2D(2, 2),
+#Now flatten the output. After this you'll just have the same DNN structure as the non convolutional version
+tf.keras.layers.Flatten(),
+#The same 128 dense layers, and 10 output layers as in the pre-convolution example:
+tf.keras.layers.Dense(128, activation='relu'),
+tf.keras.layers.Dense(10, activation='softmax')
+])
+
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.fit(training_images, training_labels, epochs=5)
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+print ('Test loss: {}, Test accuracy: {}'.format(test_loss, test_acc*100))
